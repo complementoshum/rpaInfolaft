8e63a47085d7f83e5805276d8ee4fa8f609ca67f
Daniel Ortiz <dortiz@complementoshumanos.com>
2024-05-15 10:58:26 -0500

    Se elimina 2captcha

--- rpa/Infolaft.py
+++ rpa/Infolaft.py
@@ -1,5 +1,3 @@
-from twocaptcha import TwoCaptcha
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
