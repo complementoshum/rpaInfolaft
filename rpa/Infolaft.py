62f1c414081f1174225983cfc73f2a0176688cc4
Daniel Ortiz <dortiz@complementoshumanos.com>
2024-05-16 14:44:23 -0500

    pruebas print

--- rpa/Infolaft.py
+++ rpa/Infolaft.py
@@ -124,20 +124,24 @@ def rpa(
                 )
                 waitBtnReportes.click()
 
+                print("reportes")
                 waitInputId = WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.XPATH, inputdId))
                 )
                 waitInputId.send_keys(idConsecutivo)
+                print("consecutivo")
 
                 waitBtnVerReporte = WebDriverWait(driver, 10).until(
                     EC.element_to_be_clickable((By.XPATH, btnVerReporte))
                 )
                 waitBtnVerReporte.click()
+                print("ver repote")
 
                 waitBtnAdministrar = WebDriverWait(driver, 10).until(
                     EC.element_to_be_clickable((By.XPATH, btnAdministrar))
                 )
                 waitBtnAdministrar.click()
+                print("reportes")
 
                 waitBtnAdminEntrar = WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.XPATH, btnAdminEntrar))
