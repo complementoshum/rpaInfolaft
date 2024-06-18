681a19daae48678debad34efc3add1f7f16d0a63
Daniel Ortiz <dortiz@complementoshumanos.com>
2024-06-18 12:22:04 -0500

    Se modifica driverRPA

--- includes/libs/app.py
+++ includes/libs/app.py
@@ -30,71 +30,68 @@ bloqueoHilos = threading.Lock()
 
 
 def driverRPA(driverDic):
-    driver = None
     options = Options()
     options.add_argument("--no-sandbox")
-    options.add_argument("--headless=new")
+    options.add_argument('--headless=new')
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--start-maximized")
+    options.add_argument("--start-fullscreen")
     options.add_argument("--disable-extensions")
-    options.add_argument("--disable-browser-side-navigation")
-    options.add_argument("--disable-web-security")
     options.add_argument("--disable-infobars")
-    options.add_argument("--disable-setuid-sandbox")
-    options.add_argument("--hide-scrollbars")
-    options.add_argument("--disable-software-rasterizer")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--disable-dev-tools")
     options.add_argument("--ignore-certificate-errors")
-    options.add_argument("--password-store=basic")
-    options.add_argument("--enable-automation")
-    options.add_argument("--disable-logging")
 
-    options.add_experimental_option(
-        "excludeSwitches", ["enable-automation", "enable-logging"]
-    )
+    # Evitar detección
+    options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_experimental_option("useAutomationExtension", False)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+
+    # Preferencias para evitar la detección
     chrome_prefs = {
-        "download.default_directory": os.environ.get("RUTAPROYECTO")
-    }  # DOCKER
+        "download.default_directory": os.environ.get("RUTAPROYECTO"),
+        "profile.default_content_setting_values.notifications": 2,  # Bloquear notificaciones
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+    }
     options.experimental_options["prefs"] = chrome_prefs
-    if not driverDic["loadStgy"]:
+
+    # User-Agent común
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.198 Safari/537.36"
+    options.add_argument(f"user-agent={user_agent}")
+
+    # Ajustes de estrategia de carga de página
+    if not driverDic.get("loadStgy"):
         driverDic["loadStgy"] = "normal"
     options.page_load_strategy = driverDic["loadStgy"]
-    driver = webdriver.Chrome(service=Service(), options=options)
+
+    # Ajustes de servicio para ChromeDriver
+    service = Service()
+
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # Modificaciones para evitar la detección
+    driver.execute_cdp_cmd(
+        "Page.addScriptToEvaluateOnNewDocument",
+        {
+            "source": """
+            Object.defineProperty(navigator, 'webdriver', {
+                get: () => undefined
+            });
+            window.navigator.chrome = {
+                runtime: {}
+            };
+            Object.defineProperty(navigator, 'plugins', {
+                get: () => [1, 2, 3],
+            });
+            Object.defineProperty(navigator, 'languages', {
+                get: () => ['en-US', 'en']
+            });
+        """
+        },
+    )
+
     time.sleep(2)
     return driver
-
-
-def listaActiva(listasRiesgo):
-    estadoListaCon = qMstr.getEstadoLista(listasRiesgo)[0]["estado"]
-    estadoListaActiva = os.getenv("LISTAACTIVA")
-
-    if str(estadoListaCon) == str(estadoListaActiva):
-        return True
-
-    logging.info("La lista no está 'ACTIVA', la consulta no será realizada.")
-    return False
-
-
-def esperaCargaPagina(driver, url, campoBuscar, timeWaitLoadPage=10, maxRetry=3):
-    for intento in range(maxRetry):
-        try:
-            driver.get(url)
-            WebDriverWait(driver, timeWaitLoadPage).until(
-                EC.element_to_be_clickable((By.XPATH, campoBuscar))
-            )
-            return True
-        except TimeoutException:
-            logging.warning(
-                f"Tiempo de espera excedido al cargar la página. Intento {intento + 1} de {maxRetry}"
-            )
-        except Exception as e:
-            logging.error(f"Error al cargar la página: {repr(e)}")
-            return False
-    logging.error("Se alcanzó el número máximo de intentos")
-    return False
 
 
 def removeFileIfExist(ruta):
